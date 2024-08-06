2b03d542b2c3704b1ae03c2df39df9a94a4b890d
Comnarin <Nsirinapuk@gmail.com>
2024-08-06 21:14:08 +0700

    UPDATE GRID

--- src/app/pages/officer/page.tsx
+++ src/app/pages/officer/page.tsx
@@ -1,6 +1,4 @@
-import { Login } from "~/app/pages/login/login";
 import { Officer } from "~/app/pages/officer/officer";
-import { Navbar } from "~/app/_components/Navbar";
 import "assets/css/main.css";
 
 export default function home() {
