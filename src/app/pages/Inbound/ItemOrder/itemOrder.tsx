4bc1e39b4e5361f9388908ea60ee51ddc0ca069c
tirawat1 <tirawat.pst@gmail.com>
2024-08-30 17:51:41 +0700

    itemOrder

---
+++ src/app/pages/Inbound/ItemOrder/itemOrder.tsx
@@ -0,0 +1,173 @@
+"use client";
+import { Container } from 'postcss';
+import { useState , useEffect } from 'react';
+import { useTheme } from '~/app/_context/Theme';
+
+export default function ItemOrder() {
+    const [itemOrder, setItemOrder] = useState([]);
+    const { isDarkMode } = useTheme();
+
+    // const [loading, setLoading] = useState(true);
+    const popUpInsert = () => {
+        
+    }
+
+    // useEffect(() => {
+    //     fetch('http://localhost:3000/itemOrder')
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             setItemOrder(data);
+    //             setLoading(false);
+    //         });
+    // }, []);
+
+    // if (loading) {
+    //     return <p>Loading...</p>;
+    // }
+
+    return (            
+            <div className='p-6 bg-slate-800 grid md:grid-cols-6  justify-center min-h-screen min-w-screen' id="card">
+                <div className='col-span-2 p-2'>
+                    <form className="max-w-md mx-auto p-6 bg-slate-200 rounded-lg w-screen h-full ">
+                        <h1 className='my-3 '>Inventory Order</h1>
+                        {/* supplier name */}
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_supplier" id="floating_supplier" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label htmlFor="floating_supplier" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Supplier name</label>
+                        </div>
+                        {/* Location */}
+                        <div className="relative z-0 w-full mb-5 group">
+                            <input type="text" name="floating_location" id="floating_location" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label htmlFor="floating_location" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Location</label>
+                        </div>
+
+                        {/* tel & Company */}
+                        <div className="grid md:grid-cols-2 md:gap-6">
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="tel" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" name="floating_phone" id="floating_phone" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label htmlFor="floating_phone" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Phone number</label>
+                            </div>
+                            <div className="relative z-0 w-full mb-5 group">
+                                <input type="text" name="floating_company" id="floating_company" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label htmlFor="floating_company" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Company (Ex. Google)</label>
+                            </div>
+                        </div>
+
+                        {/* date when recieve */}
+                        <div className="relative z-0 w-full mb-5 group">
+                                <input type="date" name="floating_datetime" id="floating_datetime" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                                <label htmlFor="floating_datetime" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Date Picker</label>
+                        </div>
+
+                        {/* select warehouse */}
+                        <div className='relative z-0 w-full mb-5 group'>
+                            <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select warehouse</label>
+                            <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected>Choose a warehouse</option>
+                                <option value="A">A</option>
+                                <option value="B">B</option>
+                                <option value="C">C</option>
+                            </select>
+                        </div>  
+
+                        {/* select category */}
+                        <div className='relative z-0 w-full mb-5 group'>
+                            <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a category</label>
+                            <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option selected>Choose a category</option>
+                                <option value="drinks">Drinks</option>
+                                <option value="food">Food</option>
+                                <option value="elc">Elc</option>
+                            </select>
+                        </div>
+
+                        {/* <div className="relative z-0 w-full mb-5 group">
+                            <input type="password" name="floating_password" id="floating_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label htmlFor="floating_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
+                        </div> */}
+                        {/* <div className="relative z-0 w-full mb-5 group">
+                            <input type="password" name="repeat_password" id="floating_repeat_password" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+                            <label htmlFor="floating_repeat_password" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
+                        </div> */}
+                        
+                
+                        
+                        {/* <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button> */}
+                    </form>
+                </div>
+                
+                {/* insert item */}
+                <div className='relative z-0 w-full mb-5 group col-span-4 p-4'>
+                        <label className="block mb-2 text-sm font-medium text-white dark:text-white">Add item</label>
+                        <button onClick={popUpInsert}
+                        className='bg-green-400 hover:bg-green-300 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm w-full sm:w-auto px-2 py-0.5 text-center m-3 '>
+                           +
+                        </button>
+                    <div className="relative overflow-x-auto">
+                        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead className="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" className="px-6 py-3 rounded-s-lg">
+                                        Product name
+                                    </th>
+                                    <th scope="col" className="px-6 py-3">
+                                        Qty
+                                    </th>
+                                    <th scope="col" className="px-6 py-3 rounded-e-lg">
+                                        subtype
+                                    </th>
+                                    <th>
+                                        
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr className="bg-white dark:bg-gray-800">
+                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Apple MacBook Pro 17"
+                                    </th>
+                                    <td className="px-6 py-4">
+                                        1
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        $2999
+                                    </td>
+                                </tr>
+                                <tr className="bg-white dark:bg-gray-800">
+                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Microsoft Surface Pro
+                                    </th>
+                                    <td className="px-6 py-4">
+                                        1
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        $1999
+                                    </td>
+                                </tr>
+                                <tr className="bg-white dark:bg-gray-800">
+                                    <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        Magic Mouse 2
+                                    </th>
+                                    <td className="px-6 py-4">
+                                    1
+                                    </td>
+                                    <td className="px-6 py-4">
+                                        $99
+                                    </td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+                                <tr className="font-semibold text-gray-900 dark:text-white bg-slate-300">
+                                    <th scope="row" className="px-6 py-3 text-base">Total</th>
+                                    <td className="px-6 py-3">3</td>
+                                    <td className="px-6 py-3">21,000</td>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+                
+            </div>
+            
+    );
+}
---
+++ src/app/pages/Inbound/ItemOrder/page.tsx
@@ -0,0 +1,13 @@
+import ItemOrder from "~/app/pages/Inbound/ItemOrder/itemOrder";
+import { Login } from "~/app/pages/login/login";
+import { Navbar } from "~/app/_components/Navbar";
+import "assets/css/main.css";
+
+export default function home() {
+    return (
+        <div>
+            <ItemOrder/>
+        </div>
+    );
+
+}
